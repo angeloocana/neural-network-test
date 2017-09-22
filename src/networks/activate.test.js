7569dcf6f41f48be90c77c07b4764c3345b9af02
Angelo Ocana <angeloocana@gmail.com>
2017-09-22 16:01:19 -0400

    Add tic tac toe network example

--- src/networks/activate.test.js
+++ src/networks/activate.test.js
@@ -23,4 +23,23 @@ describe('networks propagate', () => {
     assert.ok(output[0] < 1);
     assert.ok(output[1] < 1);
   });
+
+  it('tic tac toe', () => {
+    const inputLayer = new Layer(9);
+    const hiddenLayer = new Layer(9);
+    const outputLayer = new Layer(9);
+
+    inputLayer.project(hiddenLayer);
+    hiddenLayer.project(outputLayer);
+
+    const myNetwork = new Network({
+      input: inputLayer,
+      hidden: [hiddenLayer],
+      output: outputLayer
+    });
+
+    const output = myNetwork.activate([1, 0, 0, 0, 0, 0, 0, 0, 0]);
+    console.log('tic tac toe: ', output);
+    assert.equal(output.length, 9);
+  });
 });
